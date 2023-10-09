77768937cefd34034223a3af254507de91a8cbbd
Luka Tsagareishvili <l.tsagareishvili29@gmail.com>
2023-10-09 04:38:40 +0400

    Finished Task01

---
+++ Day01/Task01/Task01/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+{
+    string input;
+    int res;
+    Console.WriteLine("Input from User:");
+    input = Console.ReadLine();
+    res = Convert.ToInt32(input);
+    Console.WriteLine("Input = {0}", res);
+
+    Random random = new Random();
+    int randNumber = random.Next(0, 10);
+
+    Console.WriteLine(randNumber);
+
+    if (randNumber == res)
+    {
+        Console.WriteLine("cifri gamoicanit");
+    }
+}
+
+
+{
+    string input;
+    int res;
+    Console.WriteLine("Input from User:");
+    input = Console.ReadLine();
+    res = Convert.ToInt32(input);
+    Console.WriteLine("Input = {0}", res);
+
+    Random random = new Random();
+    int randNumber = random.Next(0, 10);
+    Console.WriteLine(randNumber);
+
+    if (randNumber == res)
+    {
+        Console.WriteLine("cifri gamoicanit");
+    }
+}
+
+
+{
+    string input;
+    int res;
+    Console.WriteLine("Input from User:");
+    input = Console.ReadLine();
+    res = Convert.ToInt32(input);
+    Console.WriteLine("Input = {0}", res);
+
+    Random random = new Random();
+    int randNumber = random.Next(0, 10);
+    Console.WriteLine(randNumber);
+
+    if (randNumber == res)
+    {
+        Console.WriteLine("cifri gamoicanit");
+    }
+    else
+    {
+        Console.WriteLine("cifri ver gamoicanit da tamashi waaget");
+    }
+}
