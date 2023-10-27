60072d911b2a5a99addb27a5800e09fba8402d56
Luka Tsagareishvili <l.tsagareishvili29@gmail.com>
2023-10-27 16:54:01 +0400

    Dones Task04(2)

---
+++ Day04(2)/Task04(2)/Task04(2)/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Task04_2_;
+
+Calculator.Add(5, 8);
+Calculator.Subtract(1, 8);
+Calculator.Multiply(5, 8);
+Calculator.Divide(42, 8);
+
+
+
